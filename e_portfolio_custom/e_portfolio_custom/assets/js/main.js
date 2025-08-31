cba82fbc6d341446794b527bca7ebbc8a3857c2b
krishnanegi-06 <knegi4394@gmail.com>
2025-08-31 12:08:59 +0530

    Delete e_portfolio_custom/e_portfolio_custom directory

--- e_portfolio_custom/e_portfolio_custom/assets/js/main.js
+++
@@ -1,5 +0,0 @@
-// simple interactions
-document.addEventListener('DOMContentLoaded', ()=>{
-  const forms = document.querySelectorAll('form');
-  forms.forEach(f=>f.addEventListener('submit', e=>{e.preventDefault(); alert('Thanks — message simulated (demo).'); f.reset();}));
-});
